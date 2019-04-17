a7c586d8246a3f3b19d9119ed6d7b4eaf67bf964
Jasterix C <loune.calixte@gmail.com>
2019-04-17 07:17:31 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -50,7 +50,7 @@ end
 def merge_data(keys, data)
   keys.each do |name_hash|
     data.each do |hash|
-      name_hash.merge!(hash[name_hash[0]])
+      name_hash.merge!(hash[name_hash[1]])
     end
   end
 end
